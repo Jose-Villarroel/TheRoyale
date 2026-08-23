70b797d6665f2f1e88f8aad853677949c129e707
Jr-412 <jd.rojaso@javeriana.edu.co>
2026-08-22 23:43:35 -0500

    Implementacion Springboot

---
+++ src/main/java/com/theroyale/backend/model/Servicio.java
@@ -0,0 +1,61 @@
+package com.theroyale.backend.model;
+
+public class Servicio {
+
+    private Long id;
+    private String nombre;
+    private String descripcion;
+    private double precio;
+    private String imagenUrl;
+
+    public Servicio() {
+    }
+
+    public Servicio(Long id, String nombre, String descripcion, double precio, String imagenUrl) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.imagenUrl = imagenUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getImagenUrl() {
+        return imagenUrl;
+    }
+
+    public void setImagenUrl(String imagenUrl) {
+        this.imagenUrl = imagenUrl;
+    }
+}
---
+++ src/main/java/com/theroyale/backend/service/ServicioService.java
@@ -0,0 +1,27 @@
+package com.theroyale.backend.service;
+
+import com.theroyale.backend.model.Servicio;
+import com.theroyale.backend.repository.ServicioRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ServicioService {
+
+    private final ServicioRepository servicioRepository;
+
+    
+    public ServicioService(ServicioRepository servicioRepository) {
+        this.servicioRepository = servicioRepository;
+    }
+
+    public List<Servicio> listarServicios() {
+        return servicioRepository.obtenerTodos();
+    }
+
+    public Optional<Servicio> buscarPorId(Long id) {
+        return servicioRepository.obtenerPorId(id);
+    }
+}
