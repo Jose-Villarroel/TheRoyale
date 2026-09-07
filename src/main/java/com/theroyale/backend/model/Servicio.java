4c073c1664cc1e018b92989e6aa4a9ee8b892632
JuanBallesteros <juanjoseballesteros23@gmail.com>
2026-09-07 18:11:57 -0500

    organizacion model Servicio.java

--- src/main/java/com/theroyale/backend/model/Servicio.java
+++ src/main/java/com/theroyale/backend/model/Servicio.java
@@ -25,18 +25,46 @@ public class Servicio {
         this.galeriaUrls = galeriaUrls;
     }
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
-    public String getDescripcion() { return descripcion; }
-    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
-    public double getPrecio() { return precio; }
-    public void setPrecio(double precio) { this.precio = precio; }
-    public String getImagenUrl() { return imagenUrl; }
-    public void setImagenUrl(String imagenUrl) { this.imagenUrl = imagenUrl; }
-    public List<String> getCaracteristicas() { return caracteristicas; }
-    public void setCaracteristicas(List<String> caracteristicas) { this.caracteristicas = caracteristicas; }
-    public List<String> getGaleriaUrls() { return galeriaUrls; }
-    public void setGaleriaUrls(List<String> galeriaUrls) { this.galeriaUrls = galeriaUrls; }
+    public Long getId() { 
+        return id; 
+    }
+    public void setId(Long id) { 
+        this.id = id; 
+    }
+    public String getNombre() { 
+        return nombre; 
+    }
+    public void setNombre(String nombre) { 
+        this.nombre = nombre; 
+    }
+    public String getDescripcion() { 
+        return descripcion; 
+    }
+    public void setDescripcion(String descripcion) { 
+        this.descripcion = descripcion; 
+    }
+    public double getPrecio() { 
+        return precio; 
+    }
+    public void setPrecio(double precio) { 
+        this.precio = precio; 
+    }
+    public String getImagenUrl() { 
+        return imagenUrl; 
+    }
+    public void setImagenUrl(String imagenUrl) { 
+        this.imagenUrl = imagenUrl; 
+    }
+    public List<String> getCaracteristicas() { 
+        return caracteristicas; 
+    }
+    public void setCaracteristicas(List<String> caracteristicas) { 
+        this.caracteristicas = caracteristicas; 
+    }
+    public List<String> getGaleriaUrls() { 
+        return galeriaUrls; 
+    }
+    public void setGaleriaUrls(List<String> galeriaUrls) { 
+        this.galeriaUrls = galeriaUrls; 
+    }
 }
